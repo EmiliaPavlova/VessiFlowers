9aeab29d4eb77207a9559bbc15746a5c3e2fb134
Emilia Pavlova <emilia.pavlova@gmail.com>
2016-10-27 03:48:00 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//// Set active selected href
+$(document).ready(function() {
+    $("[href]").each(function() {
+        if (this.href === window.location.href) {
+            $(this).addClass("active");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $(".dropdown-menu li [href]").each(function () {
+        if (this.href === window.location.href) {
+            $(this).addClass("active");
+            $(this).parent().parent().prev().addClass("active");
+        }
+    });
+});
